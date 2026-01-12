6e88506b0e2618fa3a3f8e51588e06d09e2cd543
diogopocas6 <81244186+diogopocas6@users.noreply.github.com>
2026-01-12 17:39:14 +0000

    Posições em pé + ciclo deitar e levantar
    Updated servo positions for calibration and adjusted loop logic for smoother movement.

--- SPIDER.cpp
+++ SPIDER.cpp
@@ -12,6 +12,13 @@
 #define SERVO_MAX  600  // pulso máximo
 #define posmin 40
 #define posmax 140
+#define zerope 45
+#define doispe 55
+#define quatrope 65
+#define oitope 25
+#define dezpe 50
+#define dozepe 30
+
 int pos;
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(PCA9685_ADDR);
 
@@ -39,38 +46,51 @@ void setup() {
 
   // Teste: mover servo do canal 0 para posição central
   pos=0;
-  moveServo(0,90);
-  moveServo(1,90);
-  moveServo(2,90);
-  moveServo(3,90);
-  moveServo(4,90);
-  moveServo(5,90);
+  moveServo(0,45);        // CALIBRADA EM PÉ -> 45
+  moveServo(1,120);       // CALIBRADA EM PÉ -> 120
+  moveServo(2,55);        // CALIBRADA EM PÉ -> 55
+  moveServo(3,90);        // CALIBRADA EM PÉ -> 90
+  moveServo(4,65);        // CALIBRADA EM PÉ -> 65
+  moveServo(5,65);        // CALIBRADA EM PÉ -> 65
   moveServo(6,90);
   moveServo(7,90);
-  moveServo(8,90);
-  moveServo(9,90);
-  moveServo(10,90);
-  moveServo(11,90);
-  moveServo(12,90);
-  delay(1000);
+  moveServo(8,25);        // CALIBRADA  EM PÉ -> 25
+  moveServo(9,115);       // CALIBRADA EM PÉ -> 115
+  moveServo(10,50);       // CALIBRADA EM PÉ -> 50
+  moveServo(11,80);       // CALIBRADA EM PÉ -> 80
+  moveServo(12,30);       // CALIBRADA EM PÉ -> 30
+  moveServo(13,90);       // CALIBRADA EM PÉ -> 90
+  moveServo(14,90); 
+  moveServo(15,90);
+  delay(3000);
 }
 
 void loop() {
   // /*
-  for (pos=posmin; pos<=posmax; pos=pos+2){
-    moveServo(0,pos);
-    delay(5);
+  for (pos=0; pos<=50; pos=pos+2){
+    moveServo(0,zerope+pos);
+    moveServo(2,doispe+pos);
+    moveServo(4,quatrope+pos);
+    moveServo(8,oitope+pos);
+    moveServo(10,dezpe+pos);
+    moveServo(12,dozepe+pos);
+    delay(50);
   }
   delay(2000);
   
-  for (pos=posmax; pos>=posmin; pos=pos-2){
-    moveServo(0,pos);
-    delay(5);
-  }
-  //   */
-  delay(2000);
   
-}
+  for (pos=0; pos<=50; pos=pos+2){
+    moveServo(0,zerope+50-pos);
+    moveServo(2,doispe+50-pos);
+    moveServo(4,quatrope+50-pos);
+    moveServo(8,oitope+50-pos);
+    moveServo(10,dezpe+50-pos);
+    moveServo(12,dozepe+50-pos);
+    delay(50);
+  }
+  delay(2000);
+  }
+//   */
 
 // Função para mover servo por ângulo (0–180)
 void moveServo(uint8_t channel, uint8_t angle) {
